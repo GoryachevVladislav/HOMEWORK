4527c24649498da91c19589577b4b52f0d9cb0ce
vlad <saymynamevlad@gmail.com>
2023-01-16 20:43:12 +0300

    первое домашнее задание

---
+++ HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n=Convert.ToInt32(Console.ReadLine ());
+Console.Write("Введите второе число: ");
+int m=Convert.ToInt32(Console.ReadLine ());
+if(n>m)
+Console.WriteLine(n);
+else if (n<m)
+Console.WriteLine(m);
+else 
+Console.WriteLine("=");
