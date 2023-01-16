05794cd8c62560df7b7ea228ca32ea7db221d360
vlad <saymynamevlad@gmail.com>
2023-01-16 20:49:34 +0300

    второе домашнее задание

---
+++ HW2/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Clear();
+ Console.Write("Введите первое число: ");
+  int n=Convert.ToInt32(Console.ReadLine ());
+   Console.Write("Введите второе число: ");
+    int m=Convert.ToInt32(Console.ReadLine ());
+     Console.Write("Введите третье число : ");
+      int b=Convert.ToInt32(Console.ReadLine ());
+       if (n>m && n>b) Console.WriteLine("Максимальное значение = "+n);
+        else if (m>b) Console.WriteLine("Максимальное значение = "+m);
+         else         
+Console.WriteLine("Максимальное значение = "+b); 
