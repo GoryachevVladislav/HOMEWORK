006d8334896722961e109e0d4ea52323fda62e62
vlad <saymynamevlad@gmail.com>
2023-01-18 10:28:08 +0300

    ДЗ

---
+++ HW4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()), count = 2;
+while (count <= a)
+{
+    Console.Write($"{count} ");
+    count = count +2;
+}
