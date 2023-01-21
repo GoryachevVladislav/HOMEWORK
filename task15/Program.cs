7e35058f3a121acf129fece5cdd5be6f7738c6dd
vlad <saymynamevlad@gmail.com>
2023-01-21 13:05:46 +0300

    задание 15

---
+++ task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<1 || n<6)
+  Console.WriteLine("будний день");
+  else
+  Console.WriteLine("выходной");
