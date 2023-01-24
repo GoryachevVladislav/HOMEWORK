c5f2a97822a9209a0a2fcf07e3a08e3904511580
vlad <saymynamevlad@gmail.com>
2023-01-24 19:49:09 +0300

    задание 19

---
+++ task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число -" );
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n/10000;
+int n2 = (n/1000)%10;
+int n4 = (n % 100)/10;
+int n5 = (n%10);
+if (n1 == n5 && n2 == n4)
+Console.WriteLine("Число является палиндропом");
+else
+Console.WriteLine("Число не является палиндропом");
