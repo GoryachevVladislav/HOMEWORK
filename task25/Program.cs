7973bde876c811695ceba5722cfd530877660cce
vlad <saymynamevlad@gmail.com>
2023-01-31 09:57:06 +0300

    задание 25

---
+++ task25/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+Console.Write("Введите первое значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе значение: ");
+int m = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= m; i++)
+result = result * n;
+Console.WriteLine(result);
