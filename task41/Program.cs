953f2cdef574f5094d0eb4e955555de1c59a82c7
vlad <saymynamevlad@gmail.com>
2023-02-05 08:51:49 +0300

    задание 41

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++; 
+}
+Console.WriteLine($"Результат: {count}");
